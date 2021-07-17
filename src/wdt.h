82cc2c5563d2ef2e97f1e86409de89a11cbced2f
Sergey Lyubka <valenok@gmail.com>
2021-07-17 13:30:12 +0100

    Update wdt_disable()

--- src/wdt.h
+++ src/wdt.h
@@ -4,18 +4,18 @@
 #if defined(ESP32C3)
 static inline void wdt_disable(void) {
   REG(C3_RTCCNTL)[42] = 0x50d83aa1;  // Disable write protection
+  // REG(C3_RTCCNTL)[36] &= BIT(31);    // Disable RTC WDT
   REG(C3_RTCCNTL)[36] = 0;           // Disable RTC WDT
+  REG(C3_RTCCNTL)[35] = 0;           // Disable
 
   // bootloader_super_wdt_auto_feed()
   REG(C3_RTCCNTL)[44] = 0x8F1D312A;
   REG(C3_RTCCNTL)[43] |= BIT(31);
   REG(C3_RTCCNTL)[45] = 0;
 
-  REG(C3_RTCCNTL)[35] = 0;      // Disable
-  REG(C3_TIMERGROUP0)[18] = 0;  // Disable T0 WDT
-  REG(C3_TIMERGROUP1)[18] = 0;  // Disable T1 WDT
-  // REG(C3_TIMERGROUP0)[18] &= BIT(31);  // Disable T0 WDT
-  // REG(C3_TIMERGROUP1)[18] &= BIT(31);  // Disable T1 WDT
+  REG(C3_TIMERGROUP0)[63] &= ~BIT(9);  // TIMG_REGCLK -> disable TIMG_WDT_CLK
+  REG(C3_TIMERGROUP0)[18] = 0;         // Disable TG0 WDT
+  REG(C3_TIMERGROUP1)[18] = 0;         // Disable TG1 WDT
 }
 #elif defined(ESP32)
 static inline void wdt_feed(void) {
