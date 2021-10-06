d1f2f7908ddace8fedf7cbb0c7b9725a4729fc3b
Sergey Lyubka <valenok@gmail.com>
2021-10-06 16:40:26 +0100

    Fill string.h

---
+++ src/libc/string.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stddef.h>
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+size_t strlen(const char *s);
--- src/sdk.h
+++ src/sdk.h
@@ -6,8 +6,8 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "gpio.h"
