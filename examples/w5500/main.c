3f88bfbd8612feb82fd661963483b7b11cb6fc0a
Sergey Lyubka <valenok@gmail.com>
2022-02-03 09:49:00 +0000

    Integrate mip

--- examples/w5500/main.c
+++ examples/w5500/main.c
@@ -1,8 +1,12 @@
 #include <sdk.h>
 #include "drivers/w5500.h"
+#include "mip.h"
 
 static int led_pin = LED1;  // To override: make EXTRA_CFLAGS=-DLED1=5
-static int led_state = 0;
+
+static void tx(struct mip_if *ifp) {
+  w5500_tx(ifp->userdata, ifp->frame, (uint16_t) ifp->frame_len);
+}
 
 int main(void) {
   wdt_disable();
@@ -14,11 +18,16 @@ int main(void) {
   struct w5500 wiz = {.spi = &spi, .cs = 5, .mac = {0xaa, 0xbb, 0xcc, 1, 2, 3}};
   w5500_init(&wiz);
 
+  uint8_t frame[2048];
+  struct mip_if mif = {.tx = tx,
+                       .userdata = &wiz,
+                       .frame = frame,
+                       .frame_max_size = sizeof(frame)};
+
   for (;;) {
-    gpio_write(led_pin, led_state);  // Blink an LED
-    sdk_log("%d\n", led_state);      // Print LED status
-    led_state ^= 1;                  // Toggle state
-    delay_ms(500);                   // Delay a bit
+    mif.frame_len = w5500_rx(&wiz, mif.frame, (uint16_t) mif.frame_max_size);
+    mip_rx(&mif);
+    mip_poll(&mif, time_us() / 1000);
   }
 
   return 0;
